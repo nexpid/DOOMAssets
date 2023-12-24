34b16256bd71e64e50bb2c97d0561a779debcd95
nexpid <nexpid@nexpid.xyz>
2023-12-24 12:23:07 +0100

    fix(workflows): AAAAAAAAAAAAAAA

--- .github/scripts/hashes.sh
+++ .github/scripts/hashes.sh
@@ -1,16 +1,8 @@
 outfile="hashes.txt"
 
-echo Sanity check 1
-
 [ -e "$outfile" ] && rm "$outfile"
 
-echo Sanity check 2
-
 find . -type f | while read -r file; do
-    # Generate MD5 hash for the file
     hash=`md5sum "$file" | awk '{print $1}'`
-    
-    # Print file name and hash to the output file
-    echo "[DEBUG]: $file"
-    echo "${file:2}?$hash" >> "$outfile"
+    echo "$file?$hash" >> "$outfile"
 done
